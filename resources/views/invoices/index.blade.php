c37b36d6fd842926403cd7ce2f2ae942764a21b3
Marcincin9 <marcino@gmx.com>
2021-01-17 14:12:10 +0000

    add invoice message

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -5,9 +5,11 @@
             <div class="container">
 
             @if (session()->has('message'))
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-              <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ session()->get('message') }}
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
             </div>    
             @endif
 
